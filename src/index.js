0b9efa84f1779c97e93227e9bea5f917ad16bec7
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-12-13 22:01:33 -0600

    Wrapped everything in try catch blocks

--- src/index.js
+++ src/index.js
@@ -2,22 +2,29 @@ import takeScreenshot from "./puppeteer.js";
 import areImagesTheSame from "./imageComparison.js"
 import uploadImage from "./cloudinary.js";
 
-
-
 export const runProcess = async () => {
+    const image1 = 'full_calendar2.png';
+    let image2;
 
-    const image1 = 'full_calendar2.png'
-    const image2 = await takeScreenshot();
-    const imagesAreTheSame = await areImagesTheSame(image1, image2);
-
-
-
-
-    if(imagesAreTheSame){
-        console.log('Images are the same');
-    } else {
-        console.log('Images are different, uploading new image');
-        uploadImage(image2);
+    try {
+        image2 = await takeScreenshot();
+    } catch (error) {
+        console.error('Error taking screenshot:', error.message);
+        // Future implementation: Send an email with error.message and additional details
+        return; // Exit the function cause no image.
     }
-}
 
+    try {
+        const imagesAreTheSame = await areImagesTheSame(image1, image2);
+
+        if (imagesAreTheSame) {
+            console.log('Images are the same.');
+        } else {
+            console.log('Images are different, uploading new image. Sending email');
+            uploadImage(image2);
+        }
+    } catch (error) {
+        console.error('Error during image comparison:', error.message);
+        // Future implementation: Send an email with error.message and additional details
+    }
+};
--- src/puppeteer.js
+++ src/puppeteer.js
@@ -2,24 +2,25 @@ import puppeteer from 'puppeteer';
 
 const takeScreenshot = async () => {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://myodfw.com/reserve-your-hunt');
+  try {
+      const page = await browser.newPage();
+      await page.goto('https://myodfw.com/reserve-your-hunt');
 
-  // Wait for the calendar to be rendered and visible
-  await page.waitForSelector('.cp_calendar', { visible: true });
+      // Wait for the calendar to be rendered and visible
+      await page.waitForSelector('.cp_calendar', { visible: true });
 
-  await new Promise(resolve => setTimeout(resolve, 5000));
+      await new Promise(resolve => setTimeout(resolve, 5000));
 
-  const calendarContainer = await page.$('.cp_calendar');
-  if (calendarContainer) {
-    await calendarContainer.screenshot({ path: 'full_calendar.png' });
-    const image = 'full_calendar.png';
-    return image;  
-  } else {
-    console.log('Calendar container not found or is not visible.');
+      const calendarContainer = await page.$('.cp_calendar');
+      if (!calendarContainer) {
+          throw new Error('Calendar container not found or is not visible.');
+      }
+
+      await calendarContainer.screenshot({ path: 'full_calendar.png' });
+      return 'full_calendar.png';  
+  } catch (error) {
+      throw error;
+  } finally {
+      await browser.close(); // Ensure the browser is closed in case of error or success
   }
-
-  await browser.close();
 }
-
-export default takeScreenshot;
