3522f3e10091e2660e402c278608e17177586096
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-12-17 14:33:05 -0600

    Added constants.js to handle email data

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+const successEmailSubject = 'Calendar Updated!';
+const successEmailContent = `The calendar has been updated!\nCheck it out here: https://myodfw.com/reserve-your-hunt`;
+const screenshotErrorEmailSubject = 'Error while taking screenshot!';
+const imageComparisonErrorEmailSubject = `Error during image comparison:`;
+
+export { successEmailSubject, successEmailContent, screenshotErrorEmailSubject, imageComparisonErrorEmailSubject}
--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import {uploadImage, deleteImage} from "./cloudinary.js";
 import { updateImageInFirestore, getImageFromFirestore } from "./firebase.js";
 import { downloadImage } from "./downloadImage.js";
 import { sendEmail } from "./emailHandler.js";
+
+import {successEmailSubject, successEmailContent, screenshotErrorEmailSubject, imageComparisonErrorEmailSubject} from "./constants.js";
+
 import dotenv from 'dotenv';
 dotenv.config();
+
+
+
+
+
 
 export const runProcess = async () => {
     
@@ -13,14 +21,14 @@ export const runProcess = async () => {
 
     await downloadImage(url, './base-image.png');
 
-    const image1 = 'base-image.png';
+    const image1 = 'full_calendar2.png';
     let image2;
     
     try {
         image2 = await takeScreenshot();
     } catch (error) {
         console.error('Error taking screenshot:', error.message);
-        await sendEmail(process.env.HUNTER_HERALD_EMAIL,'Error while taking screenshot!', error.message, null);
+        await sendEmail(process.env.HUNTER_HERALD_EMAIL,screenshotErrorEmailSubject, error.message, null);
         console.log('Email sent.');
         return; // Exit the function cause no image.
     }
@@ -37,7 +45,7 @@ export const runProcess = async () => {
             await updateImageInFirestore(publicId, imageUrl);
             console.log('New image updated in Firestore.');
 
-            await sendEmail(process.env.CLIENT_EMAIL,'Calendar Updated!', 'The calendar has been updated! Check it out here: https://myodfw.com/reserve-your-hunt', image2);
+            await sendEmail(process.env.CLIENT_EMAIL,successEmailSubject, successEmailContent, image2);
             console.log('Email sent.');
             
             if(oldImageData && oldImageData.publicId) {
@@ -57,7 +65,7 @@ export const runProcess = async () => {
             console.log('New image uploaded to Cloudinary.');
             await updateImageInFirestore(publicId, imageUrl);
             console.log('New image updated in Firestore.');
-            await sendEmail(process.env.CLIENT_EMAIL,'Calendar Updated!', 'The calendar has been updated! Check it out here: https://myodfw.com/reserve-your-hunt', image2);
+            await sendEmail(process.env.CLIENT_EMAIL,successEmailSubject, successEmailContent, image2);
             console.log('Email sent.');
             
             if(oldImageData && oldImageData.publicId) {
@@ -68,7 +76,7 @@ export const runProcess = async () => {
             console.log('Process completed.');
         } else {
             console.error('Error during image comparison:', error.message);
-            await sendEmail(process.env.HUNTER_HERALD_EMAIL,'Error during image comparison:', error.message, null);
+            await sendEmail(process.env.HUNTER_HERALD_EMAIL,imageComparisonErrorEmailSubject, error.message, null);
         }
     }
 };
