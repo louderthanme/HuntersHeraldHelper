10d2ffebe72459a9c4b7ce31790cf8dbe8efc328
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-12-26 13:22:18 -0600

    Added an endpoint to keep app alive

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-import cron from "node-cron";
-import { runProcess } from  "./src/index.js";
+import express from 'express';
+import cron from 'node-cron';
+import { runProcess } from './src/index.js';
 
-cron.schedule("*/5 * * * *", () => {
-    console.log("Running process...");
-    runProcess();
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Schedule the task
+cron.schedule('*/5 * * * *', () => {
+  console.log('Running process...');
+  runProcess();
+});
+
+// Basic route just to keep the server responding
+app.get('/', (req, res) => {
+  res.send("Hunter's Herald Helper is running");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
